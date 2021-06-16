768be5bccdc08feb3a30db4b89be050be3059478
Sam Lloyd <bobdoe784@gmail.com>
2021-06-16 16:10:15 -0700

    Variables for password characters have been added to main function as prompts and confirms

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,11 @@ var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  var passLength = 
+  var passLength = window.prompt('Please select the length of your passcode.')
+  var passUp = window.confirm('Do you want uppercase letters?')
+  var passLow = window.confirm('Do you want lowercase letters?')
+  var passNum = window.confirm('Do you want numbers?')
+  var passSpec = window.confirm('Do you want special characters?')
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
 
